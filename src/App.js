f62e57800f3c14a909dc79fe76698d0677565e4b
Dudkin Sergey <dsergik@mail.ru>
2023-06-08 20:54:17 +0300

    install and setting scss. Add folders. Add useState to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import Header from "./components/Header/Header";
-
+import React from 'react';
+import AddTodo from './components/AddTodo';
+import Header from './components/Header';
+import TodoList from './components/TodoList';
 
 function App() {
-  return <>
-  <Header />
-  </>
+  const [todo, setTodo] = React.useState([
+    {
+      id: 1,
+      title: 'first todo',
+      status: true,
+    },
+    {
+      id: 1,
+      title: 'first todo',
+      status: true,
+    },
+  ]);
+  return (
+    <>
+      <Header />
+      <AddTodo />
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
--- src/components/AddTodo/index.jsx
+++ src/components/AddTodo/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AddTodo() {
   return (
-    <div>AddTodo</div>
+    <div>Form</div>
   )
 }
 

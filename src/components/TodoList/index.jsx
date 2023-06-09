50cde28be216accdd2c9cf488c32762a367cc207
Dudkin Sergey <dsergik@mail.ru>
2023-06-09 21:40:44 +0300

    create button edit and function editTodo, useState edit

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,33 +1,55 @@
-import React from 'react'
+import React from 'react';
 
-function TodoList({todo, setTodo}) {
+function TodoList({ todo, setTodo }) {
+  const [edit, setEdit] = React.useState(null);
 
   const deleteTodo = (id) => {
-  let newTodo = [...todo].filter(item => item.id!==id)
-  setTodo(newTodo)
-  }
+    let newTodo = [...todo].filter((item) => item.id !== id);
+    setTodo(newTodo);
+  };
 
   const statusTodo = (id) => {
-    let newTodo = [...todo].filter(item => {
-      if(item.id === id){
-        item.status = !item.status
+    let newTodo = [...todo].filter((item) => {
+      if (item.id === id) {
+        item.status = !item.status;
       }
       return item;
-    })
+    });
     setTodo(newTodo);
-  }
+  };
 
+  const editTodo = (id) => {
+    setEdit(id);
+  };
 
   return (
-   todo.map((item) => (
-  <div key={item.id}>
-    <div>{item.title}</div>
-    <button onClick={() =>  deleteTodo(item.id)}>Удалить</button>
-    <button onClick={() =>  statusTodo(item.id)}>Закрыть</button>
-  </div>
-    
-   ))
-  )
+    <>
+      {todo.map((item) => (
+        <div key={item.id}>
+          {edit === item.id ? (
+            <div>
+              <input />
+              <button>Сохранить</button>
+            </div>
+          ) : (
+            <div>{item.title}</div>
+          )}
+          {edit === item.id ? (
+            <div>
+              <button>Сохранить</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => deleteTodo(item.id)}>Удалить</button>
+              <button onClick={() => editTodo(item.id)}>Редактировать</button>
+              <button onClick={() => statusTodo(item.id)}>Закрыть</button>
+              {/*  */}
+            </div>
+          )}
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default TodoList
+export default TodoList;
